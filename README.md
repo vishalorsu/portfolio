471f1b508e9b02c73b91fcc01dc79a8ebee37db8
vishalorsu <you@example.com>
2025-02-02 02:59:43 -0500

    Updated portfolio with new assets, images, and component modifications

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Developer Portfolio Template 🚀
+# Portfolio Template 🚀
 
 ![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB) ![Node.js](https://img.shields.io/badge/Node%20js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white) ![npm](https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white) ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white) ![JavaScript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E) ![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![Sass](https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white)
 
@@ -6,7 +6,7 @@
 
 This simple portfolio template is designed to showcase your past projects, career history, skill sets, and more.
 
-View the [Demo](https://yujisatojr.github.io/react-portfolio-template/).
+View the [Demo](https://vishalorsu.github.io/vishal.github.io/).
 
 **This template is free to use, and no attribution is required.** You can fork or download this repository to customize it for your own use. Please don't forget to leave a ⭐ if you like this portfolio!
 
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,50 +16,50 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="2024 - present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title">Data Engineer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Washington DC</h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+               Data Pipeline Development, ETL Processes, Cloud Data Management, Big Data Technologies
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="2024"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Data Scientist</h3>
+            <h4 className="vertical-timeline-element-subtitle">Fairfax, VA</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+              Frontend Development, Machine Learning, Predictive Modelling, Data Visualization 
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2021 - 2021"
+            date="2021 - 2022"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Data Engineer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Hyderabad, India</h4>
             <p>
-              Full-stack Development, API Development, User Experience
+              Data Pipeline, ETL, Cloud Engineering, Database Management
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2020"
+            date="2019 - 2020"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
             <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
+            <h4 className="vertical-timeline-element-subtitle">Hyderabad, India</h4>
             <p>
-              Automation, Data Governance, Statistical Analysis
+                Data Cleaning, Reporting, SQL, Dashboard Development
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
